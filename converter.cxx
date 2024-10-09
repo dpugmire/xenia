707691af5eba0b5c3f75980bc756f79d68b5004d
Dave Pugmire <dpugmire@gmail.com>
2024-10-09 12:41:38 -0400

    Enable MPI and some code cleanup.

--- converter.cxx
+++ converter.cxx
@@ -37,16 +37,6 @@ RunBP(const boost::program_options::variables_map& vm)
     reader.EndStep();
   }
 
-  /*
-  for (vtkm::Id step = 0; step < numSteps; step++)
-  {
-    reader.Step = step;
-    auto output = reader.ReadDataSet(step);
-    std::cout<<"Step: "<<step<<" num ds= "<<output.GetNumberOfPartitions()<<std::endl;
-
-    RunService(writer, output, vm);
-  }
-  */
   writer.Close();
 }
 
@@ -57,8 +47,9 @@ RunSST(const boost::program_options::variables_map& vm)
   xenia::utils::DataSetWriter writer(vm);
   xenia::utils::DataSetReader reader(vm);
 
+#if 0 //can we take this out??
 //old code.
-if (1)
+if (0)
 {
   std::string inputFname = vm["file"].as<std::string>();
   std::string outputFname = vm["output"].as<std::string>();
@@ -78,6 +69,7 @@ if (1)
   params["engine_type"] = "SST";
   reader.SetDataSourceParameters("source", params);
   }
+  #endif
 
   reader.Init();
   vtkm::Id step = 0;
@@ -132,7 +124,9 @@ if (1)
 
 int main(int argc, char** argv)
 {
-  //MPI_Init(NULL, NULL);
+#ifdef ENABLE_MPI
+  MPI_Init(NULL, NULL);
+#endif
 
   namespace po = boost::program_options;
 
@@ -158,70 +152,9 @@ int main(int argc, char** argv)
     RunSST(vm);
   else
     RunBP(vm);
-  //MPI_Finalize();
 
-  return 0;
-
-
-#if 0
-  if (false) //inputFname.find("sst") != std::string::npos)
-  {
-    std::cout<<"SST!!!"<<std::endl;
-    std::string jsonFile = "./contour.json", fname = "./gs.bp";
-    fides::io::DataSetReader reader(jsonFile);
-    std::unordered_map<std::string, std::string> paths;
-    paths["source"] = fname;
-
-    fides::DataSourceParams params;
-    params["engine_type"] = "SST";
-    reader.SetDataSourceParameters("source", params);
-
-    int step = 0;
-    while (true)
-    {
-      auto status = reader.PrepareNextStep(paths);
-      if (status == fides::StepStatus::NotReady)
-      {
-        std::cout << "Not ready...." << std::endl;
-        continue;
-      }
-      else if (status == fides::StepStatus::EndOfStream)
-      {
-        std::cout << "Stream is done" << std::endl;
-        break;
-      }
-
-      fides::metadata::MetaData selections;
-      //selections.Set(fides::keys::BLOCK_SELECTION(), blockSelection);
-
-      auto output = reader.ReadDataSet(paths, selections);
-      std::cout << "Step: " << step << " #ds= " << output.GetNumberOfPartitions() << std::endl;
-
-      step++;
-    }
-    return 0;
-  }
-
-  //There is an issue with the DataSetWriter. When it goes out of scope, it calls Engine.Close().
-  //This makes some MPI calls. When the object destructs after MPI_Finalize, results in an error.
-  {
-    xenia::utils::DataSetReader reader(vm);
-    xenia::utils::DataSetWriter writer(vm);
-    reader.Init();
-
-    vtkm::Id numSteps = reader.GetNumSteps();
-    for (vtkm::Id step = 0; step < numSteps; step++)
-    {
-      reader.Step = step;
-      auto output = reader.ReadDataSet(step);
-
-      std::cout<<"Step: "<<step<<" num ds= "<<output.GetNumberOfPartitions()<<std::endl;
-      writer.BeginStep();
-      writer.WriteDataSet(output);
-      writer.EndStep();
-    }
-  }
+#ifdef ENABLE_MPI
+  MPI_Finalize();
 #endif
-
   return 0;
 }
